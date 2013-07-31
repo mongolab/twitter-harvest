409ab84f747d751e2943cbff09bd084ad8e73e06
Chris Chang <chang.christopher@gmail.com>
2013-07-31 15:15:32 -0700

    changed arg names, changed exception handling

--- twitter-harvest/twitter-harvest.py
+++ twitter-harvest/twitter-harvest.py
@@ -32,7 +32,6 @@ import oauth2 as oauth
 import urllib2, json
 import sys, argparse, time 
 
-
 def oauth_header(url, consumer, token):
     params =  {'oauth_version': '1.0',
                'oauth_nonce': oauth.generate_nonce(),
@@ -40,18 +39,16 @@ def oauth_header(url, consumer, token):
               }
     req = oauth.Request(method = 'GET',url = url, parameters = params)
     req.sign_request(oauth.SignatureMethod_HMAC_SHA1(),consumer, token)
-    head = req.to_header()['Authorization'].encode('utf-8')
-    return head
-
+    return req.to_header()['Authorization'].encode('utf-8')
 
 def main():
     
     ### Build arg parser
     parser = argparse.ArgumentParser(description = 'Connects to Twitter User Timeline endpoint, retrieves tweets and inserts into a MongoLab database. Developed on Python 2.7')
     parser.add_argument('-r', '--retweet', help = 'include native retweets in the harvest', action = 'store_true')
-    parser.add_argument('-d', '--display', help = 'print harvested tweets in shell.', action = 'store_true')
+    parser.add_argument('-v', '--verbose', help = 'print harvested tweets in shell', action = 'store_true')
     parser.add_argument('--numtweets', help = 'set total number of tweets to be harvested. max = 3200', type = int, default = 3200)
-    parser.add_argument('--user', help = 'choose user timeline for harvest', default = 'mongolab')
+    parser.add_argument('--user', help = 'choose twitter user timeline for harvest', default = 'mongolab')
     parser.add_argument('--db', help = 'MongoLab DB URI, example: mongodb://dbuser:dbpassword@dbh85.mongolab.com:port/dbname', required = True)
     parser.add_argument('--consumer-key', help = 'Consumer Key from your Twitter App OAuth settings', required = True)
     parser.add_argument('--consumer-secret', help = 'Consumer Secret from your Twitter App OAuth settings', required = True)
@@ -62,7 +59,7 @@ def main():
     args = parser.parse_args()
     user = args.user 
     numtweets = args.numtweets
-    display = args.display
+    verbose = args.verbose
     retweet = args.retweet
 
     ### Build Signature
@@ -82,35 +79,46 @@ def main():
     uri_parts = pymongo.uri_parser.parse_uri(URI)
     db_name = uri_parts['database']
     db = conn[db_name]
-    db.TwitterHarvest.ensure_index("id_str", unique = True)
-    
+    db['twitter-harvest'].ensure_index("id_str", unique = True)
+  
+    ### Helper Variables for Harvest
+    max_id = -1
+    tweet_count = 0
+
     ### Begin Harvesting
     while True:
         auth = oauth_header(url, oauth_consumer, oauth_token)
         headers = {"Authorization": auth}
         request = urllib2.Request(url, headers = headers)
-        f = urllib2.urlopen(request)
-        tweets = json.load(f)
-        if 'errors' in tweets:
+        stream = urllib2.urlopen(request)
+        tweet_list = json.load(stream)
+
+        if 'errors' in tweet_list:
             print 'Hit rate limit, code: %s, message: %s' % (tweets['errors']['code'], tweets['errors']['message'])
-            sys.exit()
-        
-	max_id = -1
+       	    return
+	if max_id == -1:
+	    tweets = tweet_list
+	else:
+	    tweets = tweet_list[1:]
+	    if len(tweets) == 0:
+	        return
+	
 	for tweet in tweets:
 	    max_id = id_str = tweet["id_str"]
 	    try:
-		if numtweets == 0:
+		if tweet_count == numtweets:
 		   print 'Hit numtweets!' 
-		   sys.exit()
-		if display == True:
+		   return 
+		if verbose == True:
 		    print tweet["text"]
-		db.TwitterHarvest.save(tweet)
-		numtweets-=1
-	    except pymongo.errors.DuplicateKeyError:
-		if len(tweets) == 1:
-		    print 'No more tweets to harvest!'
-		    sys.exit()    
+		db['twitter-harvest'].save(tweet)
+		tweet_count+=1
+	    except Exception:
+                print 'Unexpected error encountered'
+		return    
 	url = base_url + "&max_id=" + max_id
+
+    print 'Finished Harvesting!'
 
 if __name__ == '__main__':
     try:
